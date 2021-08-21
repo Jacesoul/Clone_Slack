600d9e0d969e76c5e62741d24bc36022785e7bc0
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-08-22 01:26:40 +0900

    ADD : unittest & e2e test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
+
+class MockUserRepository {
+  // private 문법
+  #data = [{ id: 1, email: 'jace@gmail.com' }];
+  findOne({ where: { email } }) {
+    const data = this.#data.find((v) => v.email === email);
+    if (data) {
+      return data;
+    }
+    return null;
+  }
+}
+class MockWorkspaceMembersRepository {}
+class MockChannelMembersRepository {}
 
 describe('UsersService', () => {
   let service: UsersService;
-
+  // beforeEach는 각각의 it전에 실행된다. service를 새로 할당한다.
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService, // UsersService가 밑의 형식과 다른이유는 provide와 useClass의 값이 같기 때문이다.
+        {
+          provide: getRepositoryToken(Users),
+          useClass: MockUserRepository,
+        },
+        {
+          provide: getRepositoryToken(WorkspaceMembers),
+          useClass: MockWorkspaceMembersRepository,
+        },
+        {
+          provide: getRepositoryToken(ChannelMembers),
+          useClass: MockChannelMembersRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +46,16 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findByEmail은 이메일을 통해 유저를 찾아야함', () => {
+    expect(service.findByEmail('jace@gmail.com')).resolves.toStrictEqual({
+      email: 'jace@gmail.com',
+      id: 1,
+    });
+  });
+
+  it.todo('findByEmail은 유저를 못 찾으면 null을 반환해야 함', () => {
+    expect(service.findByEmail('jace@gmil.com')).resolves.toBe(null);
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import passport from 'passport';
+import session from 'express-session';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -12,6 +14,19 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.use(
+      session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+          httpOnly: true,
+        },
+      }),
+    );
+    app.use(passport.initialize());
+    // jwt를 써서 세션이 필요없는 경우는 아래의 세션미들웨어는 삭제해도 된다.
+    app.use(passport.session());
     await app.init();
   });
 
@@ -20,5 +35,15 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  // superagent -> supertest
+  // axios -> moxios
+  // 비동기 테스트는 done이 호출되어야 끝나는 걸로 칩니다.
+  it('/users/login (POST)', (done) => {
+    return request(app.getHttpServer())
+      .post('/api/users/login')
+      .send({ email: 'jace@gmail.com', password: 'nodejsbook' })
+      .expect(201, done);
   });
 });
